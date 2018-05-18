35a91785811e56d25ba6d3871def332ef111b8fd
duong.nguyenxuan <duong.nguyenxuan@elinext.com>
2018-05-18 10:28:44 +0700

    set up base project & module life-cycle

---
+++ lifecycle/src/main/java/com/duongnx/android/lifecylce/Test.java
@@ -0,0 +1,7 @@
+package com.duongnx.android.lifecylce;
+
+/**
+ * Created by duongnx on 18/05/2018
+ */
+public class Test {
+}
---
+++ lifecycle/src/main/java/com/duongnx/android/lifecylce/step1/ChronoActivity1.kt
@@ -0,0 +1,5 @@
+package com.duongnx.android.lifecylce.step1
+
+/**
+ * Created by duongnx on 18/05/2018
+ */
