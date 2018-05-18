c84511c43dec31c1decebeadac7665eb447546f8
duong.nguyenxuan <duong.nguyenxuan@elinext.com>
2018-05-18 14:45:36 +0700

    Lab1: chronometer will be restart when we rotate the screen

--- lifecycle/src/main/java/com/duongnx/android/lifecylce/step1/ChronoActivity1.kt
+++ lifecycle/src/main/java/com/duongnx/android/lifecylce/step1/ChronoActivity1.kt
@@ -1,8 +1,21 @@
 package com.duongnx.android.lifecylce.step1
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.duongnx.android.lifecylce.R
+import kotlinx.android.synthetic.main.activity_chronometer.*
+
 /**
  * Created by duongnx on 18/05/2018
+ * Code lab: https://codelabs.developers.google.com/codelabs/android-lifecycles
+ *
+ * Lab1: chronometer will be restart when we rotate the screen
  */
-class ChronoActivity1{
+class ChronoActivity1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chronometer)
 
+        chronometer.start()
+    }
 }
